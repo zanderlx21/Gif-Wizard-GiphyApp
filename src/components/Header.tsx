8132ca16085b6414634ada879b9e344f28812740
Alex Zaks <zaksa86@gmail.com>
2022-03-15 17:47:54 -0400

    styled to deploy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
     
     return (
         <div className="header">
-            <h1 id="App-Title">Gif Wizard<img src={wand} id="wand"/></h1>
+            <h1 id="App-Title">Gif Wizard<img src={wand} id="wand" alt="a black magic wand"/></h1>
             {/* <img src="../GiphyLogo.gif" /> */}
             <p>Powered by <a href="www.giphy.com">GIPHY</a></p>
         </div>
